19076417351cdefe78f0a7d837f78963ffe62c6b
Gianmarco_Bau <bau.gianmarco@gmail.com>
2018-11-15 09:27:41 +0100

    Inserimento IBOutlet ed IBAction

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var TextField: UITextField!
+    @IBOutlet weak var lblArea: UILabel!
+    @IBAction func btnCalcola(_ sender: Any) {
+        var lato : Int = fromStringToInt(daTradurre: TextField.text!)
+        var area : Int = calcolaAreaQuadrato(lato: lato)
+        lblArea.text = String(area)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func fromStringToInt(daTradurre: String) -> Int{
+        return Int(daTradurre)!
+    }
+    
+    func calcolaAreaQuadrato(lato: Int) -> Int{
+        return lato * lato
+    }
 }
 
